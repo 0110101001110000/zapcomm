4681ee6839d9148506de33ca9c50a79fda4dd4d4
01101010-01110000 <0110101001110000_WK@proton.me>
2024-11-29 01:05:57 -0300

    Atualiza função listen(). server.js

--- frontend/server.js
+++ frontend/server.js
@@ -2,9 +2,14 @@
 const express = require("express");
 const path = require("path");
 const app = express();
+const port = process.env.PORT || 4000;
+const host = process.env.HOST || 'localhost';
 app.use(express.static(path.join(__dirname, "build")));
 app.get("/*", function (req, res) {
 	res.sendFile(path.join(__dirname, "build", "index.html"));
 });
 //app.listen(3000);
-app.listen(10000); 
+app.listen(port, host, function(err){
+    if (err) console.log("Error in server setup")
+    console.log("Server listening on Port", port);
+});
