c4c9958bade09e647e30dca21e50865fe89454d7
01101010-01110000 <0110101001110000_WK@proton.me>
2024-11-29 13:53:26 -0300

    Adiciona propriedade Matching status em todas as tags padrões && Altera cor da tag waitingSupplier

--- frontend/src/pages/Kanban/config.js
+++ frontend/src/pages/Kanban/config.js
@@ -1,24 +1,34 @@
 
 export const defaultTags = {
-    talkingTag: {
-      name: "Em Conversa",
-      color: "#190bff99",
-      kanban: 1,
-    },
-    waitingSupplierTag: {
-      name: "Aguardando Fornecedor",
-      color: "#f4a742",
-      kanban: 1,
-    },
-    preventedTag: {
-      name: "Impedido",
-      color: "#ef686f",
-      kanban: 1,
-    },
-    finishedTag: {
-      name: "Finalizado",
-      color: "#00b97399",
-      kanban: 1,
-    }
-  };
+  /* pendingTag: {
+    name: "Aguardando",
+    color: "#d6d6d6",
+    matchingStatus: "pending", 
+    kanban: 1,
+  }, */
+  talkingTag: {
+    name: "Em Conversa",
+    color: "#190bff99",
+    matchingStatus: "open", 
+    kanban: 1,
+  },
+  waitingSupplierTag: {
+    name: "Aguardando Fornecedor",
+    color: "#f5b43c99",
+    matchingStatus: "waiting",
+    kanban: 1,
+  },
+  preventedTag: {
+    name: "Impedido",
+    color: "#ef686f",
+    matchingStatus: "prevented",
+    kanban: 1,
+  },
+  finishedTag: {
+    name: "Finalizado",
+    color: "#00b97399",
+    matchingStatus: "closed",
+    kanban: 1,
+  }
+};
   
